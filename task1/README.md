b7e04317a31d8ed656fd9a25bc91bf188a19cff8
mehdi <astarakee@gmail.com>
2023-07-24 14:39:37 +0200

    model was modified by further  corrping the input volumes to speed up the process

--- task1/README.md
+++ task1/README.md
@@ -3,11 +3,11 @@
 To build the SegRap Docker Image:
 
 ```shell
-docker build . -t seg_rap_2023
+docker build . -t seg_crop
 ```
 
 To run the prediction on new data:
 
 ```shell
-docker run --rm --gpus <GPU NUM> -v <PATH/TO/LOCAL/HEAD-NECK_CT_FOLDER>:/input/images/head-neck-ct -v <PATH/TO/LOCAL/HEAD-NECK_CE_CT_FOLDER>:/input/images/head-neck-contrast-enhanced-ct -v </PATH/TO/LOCAL/OUTPUT_FOLDER>:/output/images/head-neck-segmentation --shm-size 2g seg_rap_2023
+docker run --rm --gpus <GPU NUM> -v <PATH/TO/LOCAL/HEAD-NECK_CT_FOLDER>:/input/images/head-neck-ct -v <PATH/TO/LOCAL/HEAD-NECK_CE_CT_FOLDER>:/input/images/head-neck-contrast-enhanced-ct -v </PATH/TO/LOCAL/OUTPUT_FOLDER>:/output/images/head-neck-segmentation --shm-size 2g seg_crop
 ```
--- task1/main.py
+++ task1/main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from tools.prepare_data import main_prepare
 from tools.label_reformulate import main_reformat
+from tools.cropping_stuff import get_body_mask, get_bounding_box, get_cropped_volumes, crop_to_fullres
 
 parser = argparse.ArgumentParser(description='SegRap2023 Challenge_taks1')
 parser.add_argument('-i', type=str, help='main path to input data', required=True)
@@ -13,18 +14,48 @@ save_path_out = args.o
 main_path_in = os.path.join(main_path_in, 'images')
 save_path_out = os.path.join(save_path_out, 'images')
 
+
 def main():
+    
+    body_mask_path = os.path.join(main_path_in, 'cropped_in')
+    crop_log_path = os.path.join(main_path_in, 'crop_log')
+    fullres_in = os.path.join(main_path_in, 'model_in')
+    nnunet_in = os.path.join(main_path_in, 'nnunet_in')
+    nnunet_out = os.path.join(save_path_out, 'seg_cropped_int')
+    fullsize_seg_path = os.path.join(save_path_out ,'seg_fullres_int')
+    
     main_prepare(main_path_in, save_path_out)
-    nnunet_in = os.path.join(main_path_in, 'model_in')
-    nnunet_out = os.path.join(save_path_out, 'seg_temp1')
+    
     print('\n'*5)
-    print('Predicting the segmentation labels begins ...')
+    print(' Extracting body mask begins ...')
+    print('\n'*5)    
+    get_body_mask(fullres_in, body_mask_path)
+    
     print('\n'*5)
+    print('Calculating the BBox coordinates begins ...')
+    print('\n'*5)   
+    get_bounding_box(body_mask_path)
+    
+    print('\n'*5)
+    print('Extracting cropped volume process begins ...')
+    print('\n'*5)    
+    get_cropped_volumes(fullres_in, body_mask_path, nnunet_in, crop_log_path)
+    
+    print('\n'*5)
+    print('Segmenting the cropped volume begins ...')
+    print('\n'*5)   
     os.system('nnUNet_predict -i %s -o %s -t 606 -m 3d_fullres -tr nnUNetTrainerV2_noMirroring --disable_tta' % (nnunet_in, nnunet_out))
+    
+    print('\n'*5)
+    print('Projecting cropped mask into full resolutional masks begins ...')
+    print('\n'*5)   
+    crop_to_fullres(save_path_out, crop_log_path, fullsize_seg_path)    
+    
     print('\n'*4)
     print('Segmentation process finished successfully!')
     print('\n'*4)
     main_reformat(save_path_out)
+    
     print('\n'*4)
     print('The pipeline was executed successfully')
     return None
