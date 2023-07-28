bb5e7212edfb3194b2e275d22477c29c15429391
mehdi <astarakee@gmail.com>
2023-07-28 15:18:59 +0200

    downloading the totalseg weights

--- task1/weight_dl.py
+++ task1/weight_dl.py
@@ -1,5 +1,8 @@
 import gdown
 
 weight_gdrive = "https://drive.google.com/uc?id=1HcoyD74deen1jcOPyYB0z8iMxhD4b50y"
+total_weight = "https://drive.google.com/uc?id=17rfov8knJC6TyXiBHI4HR4ks49YI7ew6"
 dst_unzip = "/opt/app/"
+dst_unzip_totalseg = "/opt/app/"
 gdown.download(weight_gdrive, dst_unzip)
+gdown.download(total_weight, dst_unzip_totalseg)
